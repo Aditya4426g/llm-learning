a59beb9014250f1263acf7d43bccc94012f79332
Aditya4426g <adityasisodiya433@gmail.com>
2025-09-10 16:55:29 +0530

    Day 2

---
+++ python-basics/03_lecture2.py
@@ -0,0 +1,46 @@
+# str1="hi"
+# str2='hello'
+# str3="""yo"""
+# print(str3)
+
+# print("Hi"+"Hello")
+# print(len("Hi Hello"))
+
+# str="Adi"
+# print(str[0])
+
+# str="Aditya"
+# print(str[2:4])
+# print(str[-5:-2])
+
+
+# str = "I am Coder."
+# print(str.endswith("er."))
+
+
+#Conditional 
+# light = ""
+# if(light == "red"):
+#     print("Stop")
+# elif(light == "green"):
+#     print("go")
+# elif(light == "yellow"):
+#     print("look")
+# else:
+#     print("Error")
+
+# num = 5
+# if(num <10):
+#     print("Less than 10")
+# if(num >0):
+#     print("Greater than 0")
+
+age =95
+
+if(age >=18 ):
+    if(age > 80):
+        print("can't drive")
+    else:
+        print("can drive")
+else:
+    print("can drive")
---
+++ python-basics/04_Practice.py
@@ -0,0 +1,55 @@
+#WAP to input user first name and print length
+# name = input("Enter your Name : ")
+# print(len(name))
+
+#WAP to find the occurence of '$' in a String
+# str = "Aditya $i$odiya"
+# print(str.count("$"))
+
+#Grade student based on marks
+
+# marks=int(input("Enter your marks : "))
+
+# if(marks>=90):
+#     print("A")
+# elif(marks >=80):
+#     print("B")
+# elif(marks >=70):
+#     print("C")
+# else:
+#     print("D")
+
+#check even odd
+# num =int(input("Enter a Number : "))
+# if(num%2 == 0):
+#     print("Even")
+# else:
+#     print("Odd")
+
+
+#Greater Number 
+# n1=int(input("Enter 1st Number : "))
+# n2=int(input("Enter 2nd Number : "))
+# n3=int(input("Enter 3rd Number : "))
+
+# if(n1>n2):
+#     if(n1>n3):
+#         print(n1)
+#     else:
+#         print(n3)
+# elif(n2>n1):
+#     if(n2>n3):
+#         print(n2)
+#     else:
+#         print(n3)
+# else:
+#     print(n3)
+
+#Multiple of 7 or not
+
+num =int(input("Enter a number : "))
+if(num%7 == 0):
+    print("Yes")
+else:
+    print("No")
+    
